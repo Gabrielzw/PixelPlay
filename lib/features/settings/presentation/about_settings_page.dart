fe9f90ac6f3bdfc6dece9c26c3dfbfb5f3972af6
Monaka <zengjiawei5tc@gmail.com>
2026-04-03 17:11:52 +0800

    修改关于页面

--- lib/features/settings/presentation/about_settings_page.dart
+++ lib/features/settings/presentation/about_settings_page.dart
@@ -19,7 +19,6 @@ class AboutSettingsPage extends StatelessWidget {
           SettingsListItem(
             title: '版本信息',
             subtitle: 'PixelPlay $kPixelPlayDisplayVersion',
-            onTap: () => _showAboutDialog(context),
           ),
           SettingsListItem(
             title: '开源许可证',
@@ -30,39 +29,6 @@ class AboutSettingsPage extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  void _showAboutDialog(BuildContext context) {
-    showAboutDialog(
-      context: context,
-      applicationName: 'PixelPlay',
-      applicationVersion: kPixelPlayDisplayVersion,
-      applicationIcon: const _AboutIcon(),
-      children: const <Widget>[Text('本地视频播放器与媒体管理工具。')],
-    );
-  }
-}
-
-class _AboutIcon extends StatelessWidget {
-  const _AboutIcon();
-
-  @override
-  Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-
-    return Container(
-      width: 56,
-      height: 56,
-      decoration: BoxDecoration(
-        color: colorScheme.primaryContainer,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Icon(
-        Icons.play_circle_fill_rounded,
-        size: 32,
-        color: colorScheme.onPrimaryContainer,
       ),
     );
   }
